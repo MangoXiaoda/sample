04fa38f84abe73270a1bd08abcaaf5179555181a
vagrant <vagrant@homestead>
2018-12-14 07:39:58 +0000

    check one error

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'staticPagesController@help')->name('help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
