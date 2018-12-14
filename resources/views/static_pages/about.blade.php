5f4840155bcdca0c1dcf0e649352361d49f49f7f
vagrant <vagrant@homestead>
2018-12-14 03:51:04 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('titile', '关于')
+@section('content')
+    <h1>关于页22</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '帮助')
+
+@section('content')
+    <h1>帮助页33</h1>
+@stop
