a220ea9142f66679136242ab97dc3fb9256f400c
vagrant <vagrant@homestead>
2019-01-04 06:55:20 +0000

    Update Index

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -29,12 +29,12 @@
   @else
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
-      <p class="lead">
+      {{-- <p class="lead">
         你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.5">Laravel 入门教程</a> 的项目主页。
-      </p>
-      <p>
+      </p> --}}
+      {{-- <p>
         一切，将从这里开始。
-      </p>
+      </p> --}}
       <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
       </p>
